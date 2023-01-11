e21e87ac66d6c2ffb2aa56de3b722c88b4fd6e12
yoshida ryuki <59759991+ryuki0724@users.noreply.github.com>
2023-01-11 14:31:43 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function sendData() {
+            var val = document.getElementById('pass_lock').value;
+            // var pass = {
+            //     password: val
+            // }
+            document.getElementById('pass_lock').value = '';
+            
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://ryuki0724.github.io/', true);
+            xhr.setRequestHeader('Content-type', 'text/plain');
+            xhr.send(val);
+            console.log(val);
+        }
